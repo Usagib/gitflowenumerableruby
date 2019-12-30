3b2dd09c7cc1b8f721930494732f1c3d4b80fbcb
Eduardo Bonilla <eduardobonillac@gmail.com>
2019-12-30 12:13:58 -0600

    methods corrected

--- script.rb
+++ script.rb
@@ -9,6 +9,7 @@ module Enumerable
       yield(self[i])
       i += 1
     end
+    self
   end
 
   def my_each_with_index
@@ -22,76 +23,110 @@ module Enumerable
 
   def my_select
     return self.to_enum unless block_given?
-    i = 0
     res_arr = []
-    length.times do
-      res_arr << self[i] if yield(self[i]) == true
-      i += 1
+    my_each do |x|
+      res_arr << x if yield x
     end
     res_arr
   end
 
   def my_all?(arg = nil)
-    i = 0
-    length.times do
-      if arg == nil && block_given? == false
-        return false if self[i] == false
-      else
-        if block_given?
-          (return false if yield(self[i]) == false) if arg == nil
-          (return false unless yield(self[i]) is_a? arg) if arg != nil
-        else
-
-        end
-        if block_given? && arg != nil
-          return false if yield(self[i]) == false
-        end
-    end
-
-      i += 1
+    if block_given?
+      my_each { |i| return false unless yield(i) }
+    elsif arg.class == Class
+      my_each { |i| return false unless i.class == arg }
+    elsif arg.class == Regex
+      my_each { |i| return false unless i =~ arg }
+    elsif arg.nil?
+      my_each { |i| return false unless i }
+    else
+      my_each { |i| return false unless i == arg }
     end
     true
+  end
+
+  def my_any?(arg = nil)
+    if block_given?
+      my_each { |i| return true if yield(i) }
+    elsif arg.class == Class
+      my_each { |i| return true if i.class == arg }
+    elsif arg.class == Regex
+      my_each { |i| return true if i =~ arg }
+    elsif arg.nil?
+      my_each { |i| return true if i }
+    else
+      my_each { |i| return true if i == arg }
+    end
+    false
   end
 
   def my_none?
-    i = 0
-    length.times do
-      return false if yield(self[i]) == true
-
-      i += 1
+    if block_given?
+      my_each { |i| return false if yield(i) }
+    elsif arg.class == Class
+      my_each { |i| return false if i.class == arg }
+    elsif arg.class == Regex
+      my_each { |i| return false if i =~ arg }
+    elsif arg.nil?
+      my_each { |i| return false if i }
+    else
+      my_each { |i| return false if i == arg }
     end
     true
   end
 
-  def my_count
-    i = 0
-    c = 0
-    length.times do
-      c += 1 if yield(self[i]) == true
-      i += 1
+  def my_count(cnt = nil)
+    counter = 0
+    if block_given?
+      my_each { |i| counter += 1 if yield(i) == true }
+    elsif cnt.nil?
+      my_each { counter += 1 }
+    else
+      my_each { |i| counter += 1 if i == cnt }
     end
-    c
+    counter
   end
 
-  def my_map
-    i = 0
-    res_array = []
-    length.times do
-      res_array << yield(self[i]) if block_given?
-      res_array << proc.call(self[i])
-      i += 1
+  def my_map(proc = nil)
+    return self.to_enum unless block_given?
+    this = self
+    res_arr = []
+    this.my_each do |i|
+      res_arr << (block_given? ? yield(i) : proc.call(i))
     end
-    res_array
+    res_arr
   end
 
-  def my_inject(start_value)
-    my_each { |element| start_value = yield(start_value, element) }
-    start_value
+  def my_inject(*args)
+    array = to_a.dup
+    if args[0].nil?
+      memory = arr.shift
+    elsif args[1].nil? && !block_given?
+      sym = args[0]
+      memory = arr.shift
+    elsif args[1].nil? && block_given?
+      memory = args[0]
+    else
+      memory = args[0]
+      sym = args[1]
+    end
+
+    array[0..-1].my_each do |i|
+      memory = if sym
+               memory.send(sym, i)
+              else
+               yield(memory, i)
+             end
+    end
+    memo
   end
+
 end
+
 def multiply_els(arr)
-  arr.my_inject(1) { |initial, num| initial * num }
+  arr.my_inject :*
 end
+
 testa = [3, 6, 4, 1, 9, 7, 8, 2, 3]
 testb = [2, 4, 6, 8, 10]
 testc = [2, 3, 4, 8, 1, 3]
